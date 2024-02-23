a0427770ef3ee2b6885ebbc3f59cd69f59391491
uhyunjin <uhyeonjin0619@gmail.com>
2024-02-23 23:10:09 +0900

    고객 등록 페이지 연결(Get, view)

--- src/main/java/com/project/library/CustomerController.java
+++ src/main/java/com/project/library/CustomerController.java
@@ -1,8 +1,12 @@
 package com.project.library;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomerController {
-
+    @GetMapping("/registration")
+    public String main(String[] args) {
+        return "registration";
+    };
 }
