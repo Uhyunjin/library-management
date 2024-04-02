f9f322d1ea322886bc04b80da21e62c8602bfa62
유현진 <98440593+Uhyunjin@users.noreply.github.com>
2024-04-02 14:41:52 +0900

    sqlsession 사용

--- src/main/java/com/project/library/ExDAO.java
+++ src/main/java/com/project/library/ExDAO.java
@@ -5,14 +5,19 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
 
+import org.apache.ibatis.session.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+
+import java.util.*;
+
 import javax.sql.DataSource;
 import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
-import java.util.List;
+
 @Repository
 public class ExDAO {
 
@@ -20,30 +25,12 @@ public class ExDAO {
     DataSource ds;
 
     public ExDTO selectAll() {
-        ExDTO ex = null;
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        String sql = "select * from user";
-
-        try {
-            conn = ds.getConnection();
-            pstmt = conn.prepareStatement(sql); // SQL Injection공격, 성능향상
-
-            rs = pstmt.executeQuery(); //  select
-
-            if (rs.next()) {
-                ex = new ExDTO();
-                ex.setId(rs.getInt(1));
-            }
-        } catch (SQLException e) {
-            return null;
-
-//            close(rs, pstmt, conn);  //     private void close(AutoCloseable... acs) {
-        }
-
-        return ex;
+        
+    private SqlSession session;
+    private static String namespace = "com.fastcampus.ch4.dao.BoardMapper.";
+        
+    @Override
+    public List<BoardDto> selectAll() throws Exception {
+        return session.selectList(namespace+"selectAll");
     }
 }
